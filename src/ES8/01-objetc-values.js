6b7754771f3c86e0599d203334e934e4dca033b2
OLiverEverlin <a20196445@pucp.edu.pe>
2023-12-16 13:05:18 -0500

    Update OBbject values

--- src/ES8/01-objetc-values.js
+++ src/ES8/01-objetc-values.js
@@ -1,4 +1,5 @@
-//extrae los valores en un arreglo sin su key
+//extrae los valores en un arreglo sin su key, o tambien puedo extraer el key 
 
 const countries ={MX: 'Mexico', CO:'Colombia', CL: 'Chile', PE: 'Peru'};
 console.log(Object.values(countries));
+console.log(Object.keys(countries));
